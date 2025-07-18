6067ac9a9511ea2d5fb6150a9acacbac010c01ff
Samuel Fernandes <150559807+Samukix@users.noreply.github.com>
2025-07-18 11:07:29 -0300

    Update buscarCep.dart

--- buscarCep.dart
+++ buscarCep.dart
@@ -1,27 +1,31 @@
 import 'package:dio/dio.dart';
 import 'dart:io';
 
-void main () {
-   String? getCep = stdin.readLineSync();
-   print('Cep Desejado: ${getCep}');
+
+void main () async {
+   String? input = stdin.readLineSync();
+   print ('Cep Desejado:  ${input}');
+
+   if (input != null){
+   var endereco = await getCep(input);
+   } 
+   
 }
 
-    Future<void> getCep (String cep) async {
-    final dio = Dio();
-    final url = 'https://viacep.com.br/ws/$cep/json/';
-
-
+Future<void> getCep (String cep) async {
+  final dio = Dio();
+  final url = 'https://viacep.com.br/ws/$cep/json/';
   final response = await dio.get(url);
       
-      if (response.statusCode == 200) {
-        final data = response.data;
-        print('Endereço:');
-        print('${data['logradouro']}');
-        print('${data['bairro']}');
-        print('${data['estado']}');
-        print('${data['cep']}');
-      } else {
-        print('Erro ao buscar cep: ${response.statusCode}');
-      }
-  
+  if (response.statusCode == 200) {
+    final data = response.data;
+    print('Endereço:');
+    print('${data['logradouro']}');
+    print('${data['bairro']}');
+    print('${data['estado']}');
+    print('${data['cep']}');
+  } else {
+    print('Erro ao buscar cep: ${response.statusCode}');
+  }
+
 }
